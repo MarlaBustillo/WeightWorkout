604ed5666de94cd80c2ce1d5302c216da8957486
Marla Bustillo <113705911+MarlaBustillo@users.noreply.github.com>
2022-11-28 15:04:54 -0500

    Update Weight.java

--- Weight.java
+++ Weight.java
@@ -1,27 +1,48 @@
+import java.util.Scanner;
+
 public class Weight {
 
-    public static void main(String[] args) {
+    private double weight;
+    private double Idealw;
+    private static String name;
+
+    public Weight(double weight, double idealw, String name) {
+        this.weight = weight;
+        Idealw = idealw;
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
         Scanner input= new Scanner(System.in);
-        System.out.println("Please enter your name");
-        String name= input.next();
         System.out.println("Please enter your current weight");
-        double weight=input.nextDouble();
-        System.out.println("Please enter your desire weight");
-        double Idealw=input.nextDouble();
+        double weightinput=input.nextDouble();
+        this.weight = weight;
+    }
 
-        System.out.println("PLEASE MAKE SURE ALL INFORMATION IS CORRECT");
+    public double getIdealw() {
+        return Idealw;
+    }
 
-        public static Getweight(){
-            System.out.println(weight);
-        }
-        public static Getname(){
-            System.out.println(name);
-        }
-        public static Idealweight(){
-            System.out.println(Idealw);
-        }
+    public void setIdealw(double idealw) {
+        Idealw = idealw;
+    }
 
+    public static String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void Printinformation(){
+        getName();
 
 
     }
-}
+    }
+
