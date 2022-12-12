0c45b2d84071f3502e4d27be805c3df94648cb7b
Marla Bustillo <113705911+MarlaBustillo@users.noreply.github.com>
2022-12-11 19:35:32 -0500

    Update Workouts.java

--- Workouts.java
+++ Workouts.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
 
+import static java.lang.Math.max;
+
+
 public class Workouts extends Weight{
     public Workouts(double weight, double name, double Idealw) {
         super(weight, name, String.valueOf(Idealw));
@@ -11,23 +14,30 @@ public class Workouts extends Weight{
         Scanner input = new Scanner(System.in);
         System.out.println("Enter what part of your body you would like to focus on");
         System.out.println("Please choose from the following:");
-        ArrayList<String> bodypart = new ArrayList<String>();
-        bodypart.add("0. Warmup");
-        bodypart.add("1.Arms");
-        bodypart.add("2.Legs");
-        bodypart.add("3.Core");
-        bodypart.add("4.Glutes");
-        System.out.println(bodypart);
-        String Body = input.next();
+        ArrayList<String> workouts = new ArrayList<String>();
+        workouts.add("1.Arms");
+        workouts.add("2.Legs");
+        workouts.add("3.Core");
+        workouts.add("4.Glutes");
+        System.out.println(workouts);
+        int workout=input.nextInt();
         System.out.println("Enter how many months you would like to reach your desired weight");
         int Month = input.nextInt();
-        System.out.println("What is your level of experince in working out");
+        System.out.println("What is your level of experience in working out");
         String Level = input.next();
-    }
+        if(Level=="B"){
+            System.out.println("You gotta start somewhere!");
+        } else if (Level=="M") {
+            System.out.println("Let's get to it!");
+
+        }else if (Level=="E")
+            System.out.println("Go run a mile!");
+        }
+
     public static <MyList> void armWorkouts(){
         Scanner input = new Scanner(System.in);
         String input1 = input.next();
-        if(input1=="Arms"){
+        if(input1=="Upper-Body"){
             MyList arms;
             String [] arm={"Shoulder Press","Cable Biceps Bar","Chest Press"};
             System.out.println("Arm Workouts entered here");
@@ -47,7 +57,7 @@ public class Workouts extends Weight{
     public static <MyList> void legWorkouts(){
         Scanner input = new Scanner(System.in);
         String input3 = input.next();
-        if(input3=="Legs"){
+        if(input3=="Lower-Body"){
             MyList Legs;
             String [] legs={"Leg Press","Leg Extensions","Seated Leg Curl"};
             System.out.println("Leg workouts entered here");
@@ -57,7 +67,7 @@ public class Workouts extends Weight{
     public static <MyList> void cardioWorkouts(){
         Scanner input = new Scanner(System.in);
         String input4 = input.next();
-        if(input4=="Cardio"){
+        if(input4=="Cool Down Cardio"){
             MyList Cardio;
             String [] cardio={"Stair Master", "TrueForm Trainer","Treadmill"};
             System.out.println("Cardio workouts entered here");
@@ -77,6 +87,9 @@ public class Workouts extends Weight{
 
 
     }
+
+
+
 
 
 
