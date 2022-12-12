4c68772427cb2b2bc1fc2a2878d55b865dbacdb5
Marla Bustillo <113705911+MarlaBustillo@users.noreply.github.com>
2022-12-11 19:44:03 -0500

    Update HelloController.java

--- HelloController.java
+++ HelloController.java
@@ -1,40 +1,16 @@
-package com.example.weightworkout2;
+package com.example.weightworkoutfx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-import java.util.ArrayList;
-
 public class HelloController {
     @FXML
     private Label welcomeText;
-    private Label workouts;
-    private Label bodyparts;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to Weight Workouts!");
-        welcomeText.setText("Your guide to weight loss!");
-
-    }
-    protected void onWorkoutButtonClick(){
-        workouts.setText("Please enter what workouts you would like to do");
-        ArrayList<String> arms=new ArrayList<>();
-        arms.add("Pull ups");
-        arms.add("Chairdips");
-        arms.add("Bicep Curls");
+    protected void onstartButtonClick() {
+        welcomeText.setText("Welcome to WeightWorkout!");
 
 
-
-    }
-    protected void onBodypartButtonClick(){
-        ArrayList<String> bodypart = new ArrayList<String>();
-        bodypart.add("0. Warmup");
-        bodypart.add("1.Arms");
-        bodypart.add("2.Legs");
-        bodypart.add("3.Core");
-        bodypart.add("4.Glutes");
-        bodyparts.setText("Please choose from the following:");
-        bodyparts.setText(String.valueOf(bodypart));
     }
 }
